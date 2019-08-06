09a05b3cab551a3b804b8d877d8d419a75de8f86
jose molina <josmoli@gmail.com>
2019-08-06 00:30:49 -0500

    Clase 7 - Organización de tests con JUnit

--- src/test/java/com/platzi/javatests/util/StringUtilTest.java
+++ src/test/java/com/platzi/javatests/util/StringUtilTest.java
@@ -6,10 +6,22 @@ import org.junit.Test;
 public class StringUtilTest {
 
     @Test
-    public void testRepeat(){
-        Assert.assertEquals("holaholahola", StringUtil.repeat("hola", 3));
+    public void repeatStringOne(){
         Assert.assertEquals("hola", StringUtil.repeat("hola", 1));
     }
 
+    @Test
+    public void repeatStringMultipleTimes(){
+        Assert.assertEquals("holaholahola", StringUtil.repeat("hola", 3));
+    }
 
+    @Test
+    public void repeatStringZeroTimes(){
+        Assert.assertEquals("", StringUtil.repeat("hola", 0));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void repeatStringNegativeTimes(){
+       StringUtil.repeat("hola", -1);
+    }
 }
