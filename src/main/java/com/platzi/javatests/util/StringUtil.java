57c6be2cff30eb6d2b306797dc382a636c82aa82
jose molina <josmoli@gmail.com>
2019-08-06 00:15:49 -0500

    start project commit

---
+++ src/main/java/com/platzi/javatests/util/StringUtil.java
@@ -0,0 +1,16 @@
+package com.platzi.javatests.util;
+
+public class StringUtil {
+
+    public static String repeat(String str, int times)
+    {
+        String result = "";
+        for (int i = 0; i < times; i++) {
+            result += str;
+        }
+
+        return result;
+    }
+}
+
+
