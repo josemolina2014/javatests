f310502f6eddfdd6450c504ef7b910c28dc8f730
jose molina <josmoli@gmail.com>
2019-08-13 21:03:54 -0500

    Clase 10 - Análisis de los tests y mejoras

--- src/test/java/com/platzi/javatests/payments/PaymentProcessorTest.java
+++ src/test/java/com/platzi/javatests/payments/PaymentProcessorTest.java
@@ -1,5 +1,6 @@
 package com.platzi.javatests.payments;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
@@ -7,30 +8,33 @@ import static org.junit.Assert.*;
 
 public class PaymentProcessorTest {
 
+    private  PaymentGateway paymentGateway;
+    private PaymentProcessor paymentProcessor;
+
+    @Before
+    public void setup(){
+        //simular la pasarela de pago
+        paymentGateway = Mockito.mock(PaymentGateway.class);
+        paymentProcessor = new PaymentProcessor(paymentGateway);
+    }
+
     @Test
     public void paymentIsCorrect() {
 
-        //simular la pasarela de pago
-        PaymentGateway paymentGateway = Mockito.mock(PaymentGateway.class);
         //simular el llamado al requestpaymente y retornando un paymentstatus OK
         Mockito.when(paymentGateway.requestPayment(Mockito.any()))
                 .thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.OK));
 
-        PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
         assertTrue(paymentProcessor.makePayment(100));
-
     }
 
     @Test
     public void paymentIsWrong() {
 
-        //simular la pasarela de pago
-        PaymentGateway paymentGateway = Mockito.mock(PaymentGateway.class);
         //simular el llamado al requestpaymente y retornando un paymentstatus ERROR
         Mockito.when(paymentGateway.requestPayment(Mockito.any()))
                 .thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.ERROR));
 
-        PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
         assertFalse(paymentProcessor.makePayment(100));
 
     }
