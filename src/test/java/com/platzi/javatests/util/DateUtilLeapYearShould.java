296fe9de3b6d2ab7a24f13e07398f63da37dbaf5
jose molina <josmoli@gmail.com>
2019-08-17 13:26:06 -0500

    Clase 13 Ejemplos de TDD: calcular el año bisiesto

---
+++ src/test/java/com/platzi/javatests/util/DateUtilLeapYearShould.java
@@ -0,0 +1,47 @@
+package com.platzi.javatests.util;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+public class DateUtilLeapYearShould {
+    /**
+     * All years divisible by 400 ARE leap years (1600, 2000, 2400).
+     * All years divisible by 100 but not by 400 are NOT leap years (1700, 1800, 1900).
+     * All years divisible by 4 but not by 100 ARE leap years (1996, 2004, 2008).
+     * All years are not divisible by 4 are NOT leap years (2017, 2018, 2019).
+     */
+
+    @Test
+    public void returnTrueWhenYearIsDivisibleBy400() {
+
+        assertThat(DateUtil.isLeapYear(1600), is(true));
+        assertThat(DateUtil.isLeapYear(2000), is(true));
+        assertThat(DateUtil.isLeapYear(2400), is(true));
+    }
+
+    @Test
+    public void returnFalseWhenYearIsDivisibleBy100ButNotBy400() {
+
+        assertThat(DateUtil.isLeapYear(1700), is(false));
+        assertThat(DateUtil.isLeapYear(1800), is(false));
+        assertThat(DateUtil.isLeapYear(1900), is(false));
+    }
+
+    @Test
+    public void returnTrueWhenYearIsDivisibleBy4ButNotBy100() {
+        assertThat(DateUtil.isLeapYear(1996), is(true));
+        assertThat(DateUtil.isLeapYear(2004), is(true));
+        assertThat(DateUtil.isLeapYear(2008), is(true));
+    }
+
+    @Test
+    public void returnFalseWhenYearIsNotDivisibleBy4() {
+
+        assertThat(DateUtil.isLeapYear(2017), is(false));
+        assertThat(DateUtil.isLeapYear(2018), is(false));
+        assertThat(DateUtil.isLeapYear(2019), is(false));
+    }
+}
